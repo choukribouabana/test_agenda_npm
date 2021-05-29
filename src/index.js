f37a601d1160301a4bfd180ea9ba2771662a6a9c
Choukri <choukri.bouabana@gmail.com>
2021-05-29 20:53:06 +0200

    Update the project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Agenda from "./agenda";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Agenda />
   </React.StrictMode>,
   document.getElementById('root')
 );
